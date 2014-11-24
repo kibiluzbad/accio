699c5850dc2b93d9e7503ee7626f3ca0143b871c
Leonardo Cardoso <kibiluzbad@gmail.com>
2014-11-24 21:26:44 -0200

    Removing db

--- infra/db.js
+++ infra/db.js
@@ -3,7 +3,7 @@
 const
     Datastore = require('nedb'),
     path = require('path'),
-    db = new Datastore({ filename: path.join(__dirname,'.db/accio'), autoload: true });
+    db = new Datastore({ filename: path.join(__dirname,'../.db/accio'), autoload: true });
 
 module.exports = db;
 
